68f4f0b05112f846bfeb45cf3f7f170fdc952d18
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-12-07 20:23:10 +0100

    add tables

--- src/table_maker.cpp
+++ src/table_maker.cpp
@@ -9,10 +9,6 @@ int main(int argc, char* argv[]) {
         std::cerr << "Choose an option: --prob || --ratio num\n";
         return 1;
     }
-    if (std::strcmp(argv[1], "--prob") != 0 && std::strcmp(argv[1], "--ratio") != 0) {
-        std::cerr << "Select a valid option: --prob || --ratio\n";
-        return 1;
-    }
     int const max_iterations = 1e6;
     int const num = std::stoi(argv[2]);
     for (int k = 0; k != num + 1; k++) {
@@ -20,25 +16,41 @@ int main(int argc, char* argv[]) {
         else std::cout << k << ',';
     }
     std::cout << std::endl;
-    for (int i = 0; i != num; i++) {
-        std::cout << i + 1;
-        for (int j = 0; j != num; j++) {
-            Battle battle{i + 1, j + 1};
-            int counter{0};
-            for (int it = 0; it != max_iterations; it++) {
-                bool winner = battle.winner();
-                if (winner == true) counter++;
-            }
-            if (std::strcmp(argv[1], "--prob") == 0) {
+    if (std::strcmp(argv[1], "--prob") == 0) {
+        for (int i = 0; i != num; i++) {
+            std::cout << i + 1;
+            for (int j = 0; j != num; j++) {
+                Battle battle{i + 1, j + 1};
+                int counter{0};
+                for (int it = 0; it != max_iterations; it++) {
+                    bool winner = battle.winner();
+                    if (winner == true) counter++;
+                }
                 std::cout << ',' << (double)counter / max_iterations;
-            } else if (std::strcmp(argv[1], "--ratio") == 0) {
+            }
+            std::cout << std::endl;   
+        }
+        return 0;
+    } else if (std::strcmp(argv[1], "--ratio") == 0) {
+        for (int i = 0; i != num; i++) {
+            std::cout << i + 1;
+            for (int j = 0; j != num; j++) {
+                Battle battle{i + 1, j + 1};
+                int counter{0};
+                for (int it = 0; it != max_iterations; it++) {
+                    bool winner = battle.winner();
+                    if (winner == true) counter++;
+                }
                 double atk_losses = (double)battle.getAtkLosses() / max_iterations;
                 double def_losses = (double)battle.getDefLosses() / max_iterations;
                 double ratio = atk_losses / def_losses;
                 std::cout << ',' << ratio << "/1";
             }
+            std::cout << std::endl;   
         }
-        std::cout << std::endl;   
+        return 0;
+    } else {
+        std::cerr << "Select a valid option: --prob || --ratio\n";
+        return 1;
     }
-    return 0;
 }
